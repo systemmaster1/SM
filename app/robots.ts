41e012bbbfe8f38adb063489aee44a5aef567bab
systemmaster1 <connect@systemmaster.in>
2026-08-07 14:39:27 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,13 @@
 import type {MetadataRoute} from 'next';
-export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://systemmaster.in/sitemap.xml'}}
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/legacy_backup/']
+    },
+    sitemap: 'https://systemmaster.in/sitemap.xml',
+    host: 'https://systemmaster.in'
+  };
+}
